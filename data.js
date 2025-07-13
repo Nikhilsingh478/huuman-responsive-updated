cd1eae533e48063b2de8b425d241e56a6e4dcf27
Nikhilsingh478 <neelsingh45940s@gmail.com>
2025-07-13 21:44:16 +0530

    finalized changes

---
+++ data.js
@@ -0,0 +1,94 @@
+// data.js
+
+const productData = [
+  {
+    id: 1,
+    name: "Discharge My Soul",
+    price: "$XXXX",
+    images: [
+      "./productimages/character1.webp",
+      "./productimages/title1.webp",
+      "./huuman.in.jpg"
+    ]
+  },
+  {
+    id: 2,
+    name: "Trouble Blue Sweatshirt",
+    price: "$XXXX",
+    images: [
+      "./productimages/character2.webp",
+      "./productimages/title2.webp",
+      "./huuman.in.jpg"
+    ]
+  },
+  {
+    id: 3,
+    name: "Black Sweatshirt",
+    price: "$XXXX",
+    images: [
+      "./productimages/character3.webp",
+      "./productimages/title3.webp",
+      "./huuman.in.jpg"
+    ]
+  },
+  {
+    id: 4,
+    name: "Beige Sweatshirt Vault Edition",
+    price: "$XXXX",
+    images: [
+      "./productimages/character4.webp",
+      "./productimages/title4.webp",
+      "./huuman.in.jpg"
+    ]
+  },
+  {
+    id: 5,
+    name: "Pink Sweatshirt New Arrival",
+    price: "$XXXX",
+    images: [
+      "./productimages/character5.webp",
+      "./productimages/title5.webp",
+      "./huuman.in.jpg"
+    ]
+  },
+  {
+    id: 6,
+    name: "Brown Lust Oversized Tee",
+    price: "$XXXX",
+    images: [
+      "./productimages/character6.webp",
+      "./productimages/title6.webp",
+      "./huuman.in.jpg"
+    ]
+  },
+  {
+    id: 7,
+    name: "Red Oversized Tee",
+    price: "$XXXX",
+    images: [
+      "./productimages/character7.webp",
+      "./productimages/title7.webp",
+      "./huuman.in.jpg"
+    ]
+  },
+  {
+    id: 8,
+    name: "Narcissist Oversized Tee",
+    price: "$XXXX",
+    images: [
+      "./productimages/character8.webp",
+      "./productimages/title8.webp",
+      "./huuman.in.jpg"
+    ]
+  },
+  {
+    id: 9,
+    name: "Black Oversized Tee",
+    price: "$XXXX",
+    images: [
+      "./productimages/character9.webp",
+      "./productimages/title9.webp",
+      "./huuman.in.jpg"
+    ]
+  }
+];
--- gsapscript.js
+++ gsapscript.js
@@ -2,38 +2,38 @@
 
 // Incoming animation for anchor tags
 gsap.from("#nav a", {
-    opacity: 0,
-    y: 50,
-    delay:5.6,
-    duration: 1.5,
-    ease: "power4.out",
-    stagger: 0.15
+  opacity: 0,
+  y: 50,
+  delay: 5.6,
+  duration: 1.5,
+  ease: "power4.out",
+  stagger: 0.15,
 });
 
 // Animation for paragraphs
 gsap.from("p", {
-    opacity: 0,
-    x: -100,
-    duration: 1.8,
-    delay:5.6,
-    ease: "power3.out"
+  opacity: 0,
+  x: -100,
+  duration: 1.8,
+  delay: 5.6,
+  ease: "power3.out",
 });
 
 // Alternative paragraph animations
 gsap.from(".elem", {
-    opacity: 0,
-    y: 100,
-    duration: 1.8,
-    delay:5.6,
-    ease: "power3.out"
+  opacity: 0,
+  y: 100,
+  duration: 1.8,
+  delay: 5.6,
+  ease: "power3.out",
 });
 
-gsap.from("button, #back", {
-    opacity: 0,
-    scale: 0.5,
-    duration: 2,
-    delay:5.6,
-    ease: "elastic.out(1, 0.75)"
+gsap.from(" #back", {
+  opacity: 0,
+  scale: 0.5,
+  duration: 2,
+  delay: 5.6,
+  ease: "elastic.out(1, 0.75)",
 });
 
 // Animation for image moving from back to front along the z-axis
@@ -46,66 +46,63 @@ gsap.from("button, #back", {
 
 // Animation for images
 gsap.from(".logo", {
-    opacity: 0,
-    scale: 0.8,
-    duration: 2.2,
-    ease: "back.out(1.5)"
+  opacity: 0,
+  scale: 0.8,
+  duration: 2.2,
+  ease: "back.out(1.5)",
 });
 
 // Animation for image container
 gsap.from("#imagediv", {
-    opacity: 0,
-    y: -100,
-    duration: 2.5,
-    ease: "power4.out"
+  opacity: 0,
+  y: -100,
+  duration: 2.5,
+  ease: "power4.out",
 });
 
-// huuman_official feature 
+// huuman_official feature
 gsap.to("#page2 h1", {
-    transform: "TranslateX(-270%)",
-    scrollTrigger: {
-        trigger: "#page2",
-        scroller: "body",
-        start: "top 0%",
-        end: "top -100%",
-        scrub: 5,
-        pin: true
-    }
-})
+  transform: "TranslateX(-270%)",
+  scrollTrigger: {
+    trigger: "#page2",
+    scroller: "body",
+    start: "top 0%",
+    end: "top -100%",
+    scrub: 5,
+    pin: true,
+  },
+});
 // huuman_official feature ends
 
-
-// bottom section animations 
+// bottom section animations
 gsap.from(".help a", {
-    // opacity: 0,
-    x: -150,
-    duration: 1,
-    ease: "power4.out",
-    stagger: 0.05,
-    scrollTrigger: {
-        trigger: ".help a",
-        scroller: "body",
-        scrub: 5,
-        start: "top 95%",
-        end: "top 90%"
-    }
-
+  // opacity: 0,
+  x: -150,
+  duration: 1,
+  ease: "power4.out",
+  stagger: 0.05,
+  scrollTrigger: {
+    trigger: ".help a",
+    scroller: "body",
+    scrub: 5,
+    start: "top 95%",
+    end: "top 90%",
+  },
 });
 
 gsap.from(".ig h2", {
-    // opacity: 0,
-    x: 150,
-    duration: 1,
-    ease: "power4.out",
-    stagger: 1,
-    scrollTrigger: {
-        trigger: ".ig h2",
-        scroller: "body",
-        scrub: 5,
-        start: "top 95%",
-        end: "top 90%"
-    }
-
+  // opacity: 0,
+  x: 150,
+  duration: 1,
+  ease: "power4.out",
+  stagger: 1,
+  scrollTrigger: {
+    trigger: ".ig h2",
+    scroller: "body",
+    scrub: 5,
+    start: "top 95%",
+    end: "top 90%",
+  },
 });
 
 // gsap.from(".ig a", {
@@ -126,108 +123,95 @@ gsap.from(".ig h2", {
 // });
 
 gsap.from(".ig a", {
-    opacity: 0,
-    x: -250,
-    duration: 1,
-    ease: "power4.out",
-    stagger: 0.10,
-    scrollTrigger: {
-        trigger: ".ig a",
-        scroller: "body",
-        scrub: 5,
-        start: "top 98%",
-        end: "top 94%"
-    }
+  opacity: 0,
+  x: -250,
+  duration: 1,
+  ease: "power4.out",
+  stagger: 0.1,
+  scrollTrigger: {
+    trigger: ".ig a",
+    scroller: "body",
+    scrub: 5,
+    start: "top 98%",
+    end: "top 94%",
+  },
 });
 
-
-
-
-
-
-
 gsap.from(".sleft h2", {
-    opacity: 0,
-    x: -250,
-    duration: 1,
-    ease: "power4.out",
-    scrollTrigger: {
-        trigger: ".sleft h2",
-        scroller: "body",
-        scrub: 5,
-    }
+  opacity: 0,
+  x: -250,
+  duration: 1,
+  ease: "power4.out",
+  scrollTrigger: {
+    trigger: ".sleft h2",
+    scroller: "body",
+    scrub: 5,
+  },
 });
 
 gsap.from(".sright .simage", {
-    opacity: 0,
-    x: 250,
-    duration: 1,
-    ease: "power4.out",
-    scrollTrigger: {
-        trigger: ".sright .simage",
-        scroller: "body",
-        scrub: 5,
-    }
+  opacity: 0,
+  x: 250,
+  duration: 1,
+  ease: "power4.out",
+  scrollTrigger: {
+    trigger: ".sright .simage",
+    scroller: "body",
+    scrub: 5,
+  },
 });
-// bottom section animation ends 
-
+// bottom section animation ends
 
 gsap.from(".shopnow h1", {
-    opacity: 0,
-    x: -250,
-    duration: 1,
-    ease: "power4.out",
-    scrollTrigger: {
-        trigger: ".shopnow h1",
-        scroller: "body",
-        scrub: 5,
-    }
+  opacity: 0,
+  x: -250,
+  duration: 1,
+  ease: "power4.out",
+  scrollTrigger: {
+    trigger: ".shopnow h1",
+    scroller: "body",
+    scrub: 5,
+  },
 });
 
-
-// gsap.from(".testimonial",{
-//     x:-200,
-//     duration:1,
-//     opacity:0,
-//     ease:"power4.out",
-//     scrollTrigger: {
-//         trigger: ".testimonials-grid",
-//         scroller: "body",
-//         scrub: 5,
-//     }
-
-// })
-
-
+gsap.from(".heading", {
+  x: -200,
+  duration: 1,
+  opacity: 0,
+  ease: "power4.out",
+  scrollTrigger: {
+    trigger: ".testimonial-header",
+    scroller: "body",
+    scrub: 5,
+  },
+});
 
 gsap.from(".title", {
-    // opacity: 0,
-    x: 250,
-    duration: 1,
-    ease: "power4.out",
-    stagger: 1,
-    scrollTrigger: {
-        trigger: ".title",
-        scroller: "body",
-        scrub: 5,
-        start: "top 70%",
-        end: "top 40%"
-    }
-
+  // opacity: 0,
+  x: 250,
+  duration: 1,
+  ease: "power4.out",
+  stagger: 1,
+  scrollTrigger: {
+    trigger: ".title",
+    scroller: "body",
+    scrub: 5,
+    start: "top 70%",
+    end: "top 40%",
+  },
 });
 
 gsap.from(".subtitle", {
-    // opacity: 0,
-    x: -250,
-    duration: 1,
-    ease: "power4.out",
-    stagger: 1,
-    scrollTrigger: {
-        trigger: ".subtitle",
-        scroller: "body",
-        scrub: 5,
-        start: "top 70%",
-        end: "top 40%"
-    }
-
+  // opacity: 0,
+  x: -250,
+  duration: 1,
+  ease: "power4.out",
+  stagger: 1,
+  scrollTrigger: {
+    trigger: ".subtitle",
+    scroller: "body",
+    scrub: 5,
+    start: "top 70%",
+    end: "top 40%",
+  },
 });
